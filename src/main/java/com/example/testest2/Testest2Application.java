efb5b011d2d192caa3dfd5d8f7cfb5b83e22f83a
UsmanSharif0001 <ussh0001@kea.stud.dk>
2025-01-08 10:13:06 +0100

    Initial commit

---
+++ src/main/java/com/example/testest2/Testest2Application.java
@@ -0,0 +1,13 @@
+package com.example.testest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Testest2Application.class, args);
+    }
+
+}
