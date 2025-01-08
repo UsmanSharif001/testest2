8122584c26d3871137b77862a157b438060b52d0
UsmanSharif0001 <ussh0001@kea.stud.dk>
2025-01-08 10:23:56 +0100

    first commit

--- src/main/java/com/example/testest2/Testest2Application.java
+++ src/main/java/com/example/testest2/Testest2Application.java
@@ -2,9 +2,19 @@ package com.example.testest2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Testest2Application {
+
+    @GetMapping("/welcomemessage")
+    String wlcmes(){
+        return "velkommen til mit springprojekt";
+    }
+
+    
 
     public static void main(String[] args) {
         SpringApplication.run(Testest2Application.class, args);
